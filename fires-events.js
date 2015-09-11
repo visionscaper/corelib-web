38aacaa5a55e44cd771bebc2aa3675bef8a736dc
Freddy Snijder <freddy@visionscapers.com>
2015-09-11 17:24:55 +0200

    Added functionality for global event handlers to FiresEvents mixin

--- fires-events.js
+++ fires-events.js
@@ -16,79 +16,124 @@
 
         /**
          *
-         * FiresEvents Mixin. Adds the methods 'event' to fire events and the
-         * method 'on' to attach handlers for those events.
+         * FiresEvents Mixin. Adds the methods :
+         *  - 'fire' to fire events,
+         *  - 'on' to attach handlers for those events.
+         *  - 'onAnyEvent' to attach global event handler, called for any event
          *
          * @class   Configurable
          * @module  M*C
          *
          */
 
-         _handlers: undefined,
+        _handlers       : undefined,
+        _globalHandlers : undefined,
 
-         /**
-          * Fire a specific event and call all registered functions for that
-          * event.
-          * @param {string} event Event name
-          * @param {object} data Event-specific data that should be passed to
-          * all registered functions.
-          * @returns {undefined}
-          */
-         event: function(event, data) {
-             var handlerList = _.obj(this._handlers) ? this._handlers[event] : null;
-             if(!_.isArray(handlerList)) {
-                 // Nothing to do
-                 return;
-             }
+        /**
+         * Fire a specific event and call all registered functions for that
+         * event.
+         * @param {string} event Event name
+         * @param {object} data Event-specific data that should be passed to
+         * all registered functions.
+         * @returns {undefined}
+         */
+        fire: function(event, data) {
+            //Not very DRY, but fast.
 
-             var i = handlerList.length;
-             while (i--) {
-                 handlerList[i](data);
-             }
-         },
-         
-         /**
-          * Add a function to handle a specific event.
-          * @param {string} event Event name
-          * @param {function} func A function that takes an object of event data 
-          * as an argument.
-          * @returns {undefined}
-          */
-         on: function(event, func) {
-             var iName   = _.exec(this, "getIName") || "UNKNOWN INSTANCE";
-             var me      = "[{}]FiresEvents::on".fmt(iName);
+            //Specific event handlers first, after that the global event handlers are called
+            var handlerList = _.obj(this._handlers) ? this._handlers[event] : null;
+            if(!_.isArray(handlerList)) {
+                // Nothing to do
+                return;
+            }
 
-             var success = false;
+            var i = handlerList.length;
+            while (i--) {
+                handlerList[i](data);
+            }
 
-             if (_.empty(event)) {
-                 _l.error(me, ("Given event name is not valid, " +
-                               "unable to add handler for event").fmt(event));
-                 return success;
-             }
-             if (!_.func(func)) {
-                 _l.error(me, ("Given event handler is not a function, " +
-                 "unable to add handler for event [{0}]").fmt(event));
-                 return success;
-             }
+            //Inform global event handlers
+            handlerList = this._globalHandlers;
+            if(!_.isArray(handlerList)) {
+                // Nothing to do
+                return;
+            }
 
-             if(!_.obj(this._handlers)) {
-                 this._handlers = {};
-             }
-             if(!_.isArray(this._handlers[event])) {
-                 this._handlers[event] = [];
-             }
+            i = handlerList.length;
+            while (i--) {
+                handlerList[i](event, data);
+            }
+        },
 
-             this._handlers[event].push(func);
-             return (success = true);
-         }
+        /**
+         * Add a function to handle a specific event.
+         * @param {string} event    Event name
+         * @param {function} func   A function that takes an object of event data
+         * as an argument.
+         *
+         * @returns {boolean}
+         */
+        on: function(event, func) {
+            var iName   = _.exec(this, "getIName") || "UNKNOWN INSTANCE";
+            var me      = "[{}]FiresEvents::on".fmt(iName);
+
+            var success = false;
+
+            if (_.empty(event)) {
+                _l.error(me, ("Given event name is not valid, " +
+                "unable to add handler for event").fmt(event));
+                return success;
+            }
+            if (!_.func(func)) {
+                _l.error(me, ("Given event handler is not a function, " +
+                "unable to add handler for event [{0}]").fmt(event));
+                return success;
+            }
+
+            if(!_.obj(this._handlers)) {
+                this._handlers = {};
+            }
+            if(!_.isArray(this._handlers[event])) {
+                this._handlers[event] = [];
+            }
+
+            this._handlers[event].push(func);
+            return (success = true);
+        },
+
+        /**
+         * Add a global handler to handle any fired event
+         *
+         * @param {function} func   function(event, eventData)
+         *
+         * @returns {Boolean}       True on success, else false
+         */
+        onAnyEvent : function(func) {
+            var iName   = _.exec(this, "getIName") || "UNKNOWN INSTANCE";
+            var me      = "[{0}]::FiresEvents::onAnyEvent".fmt(iName);
+
+            var success = false;
+
+            if (!_.func(func)) {
+                _l.error(me, "Given global event handler is not a function, " +
+                              "unable to add global handler");
+
+                return success;
+            }
+
+            if(!_.array(this._globalHandlers)) {
+                this._globalHandlers = [];
+            }
+
+            this._globalHandlers.push(func);
+            return (success = true);
+        }
 
         /***************************************************
          *
          * PROTECTED METHODS
          *
          **************************************************/
-
-        
 
     });
 })();
